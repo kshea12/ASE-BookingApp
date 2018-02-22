210002c69cbcd97655e6a226c195b5d4b96d3797
Kevin Shea <kshea@njmrsnet-aegis.com>
2018-02-22 16:18:06 -0500

    Users collection now pulls over all data and metadata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,19 @@ export class AppComponent {
   title = 'ASE Booking App';
   user: Observable<firebase.User>;
   users: Observable<any[]>;
+  user_collection: any;
 
-  constructor(public afAuth: AngularFireAuth, database: AngularFirestore) {
+  constructor(public afAuth: AngularFireAuth, private afs: AngularFirestore) {
     this.afAuth.auth.signInAnonymously();
     this.user = this.afAuth.authState;
-    this.users = database.collection('users').valueChanges();
+    // this.users = afs.collection('users').valueChanges();
+    this.user_collection = this.afs.collection('users');
+    this.users = this.user_collection.snapshotChanges().map(actions => {
+      return actions.map(a => {
+        const data = a.payload.doc.data();
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      });
+    });
   }
 }
